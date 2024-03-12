aedfa3a655ad74aeb2628e49855a7009fa33d705
Hoseinpy <hoosinkhalili66@gmail.com>
2024-03-12 23:51:39 +0330

    started food_shopping project and create login api

---
+++ README.md
@@ -0,0 +1,5 @@
+### django food_shopping
+__________________________________________________
+
+hello guys this is new food_shopping project i started
+used django_rest_framework for this project
---
+++ auth_api/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from django.contrib.auth import authenticate, login, logout
+from .models import UserModel
+from .serializers import LoginSerializer
+
+
+class LoginApiView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.data.get('username'), password=serializer.data.get('password'))
+            if user is not None:
+                current_user = user
+                login(request, current_user)
+                return Response({'status': 'login successful'}, status.HTTP_200_OK)
+            else:
+                return Response({'status': 'incurrect username or password'}, status.HTTP_404_NOT_FOUND)
+        else:
+            return Response({'status': 'bad request'}, status.HTTP_400_BAD_REQUEST)
