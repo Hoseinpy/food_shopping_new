b20678b81b9d859361332751f0875c1e432ff087
Hoseinpy <hoosinkhalili66@gmail.com>
2024-03-15 19:57:02 +0330

    create profile api, fix some bug

--- auth_api/urls.py
+++ auth_api/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import LoginApiView, SingupApiView, LogoutApiView
+from .views import LoginApiView, SingupApiView, LogoutApiView, ProfileApiView
 
 urlpatterns = [
     path('login/', LoginApiView.as_view(), name='login-api'),
     path('singup/', SingupApiView.as_view(), name='singup-api'),
-    path('logout/', LogoutApiView.as_view(), name='logout-api')
+    path('logout/', LogoutApiView.as_view(), name='logout-api'),
+    path('profile/<code>', ProfileApiView.as_view(), name='profile-api'),
 ]
