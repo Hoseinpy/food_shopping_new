ac6a8935c70e0222e702bce308c904283bf53374
Hoseinpy <hoosinkhalili66@gmail.com>
2024-03-17 15:37:26 +0330

    add fianll price system and fix add cart bug

--- products/views.py
+++ products/views.py
@@ -9,6 +9,7 @@ from django_ratelimit.decorators import ratelimit
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from utils.total_price import total_price
+from rest_framework import mixins
 
 
 # this api show all food in database.
@@ -74,7 +75,7 @@ class CartListApiView(APIView):
 
 # this api show detail and can delete and update cart
 @method_decorator([ratelimit(key='ip', rate='20/m'), csrf_exempt], name='dispatch')
-class CartDetailsApiView(APIView):
+class CartDetailsApiView(APIView, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
     authentication_classes = [TokenAuthentication]
 
     def get(self, request, cart_id):
@@ -82,7 +83,6 @@ class CartDetailsApiView(APIView):
             cart = CartModel.objects.filter(id=cart_id).first()
             if cart:
                 serializer = CartSerializer(cart)
-                # todo: add fianll price
                 return Response(serializer.data, status.HTTP_200_OK)
             else:
                 return Response({'status': 'not found'}, status.HTTP_404_NOT_FOUND)
@@ -94,6 +94,10 @@ class CartDetailsApiView(APIView):
             cart = get_object_or_404(CartModel, id=cart_id)
             serializer = CartSerializer(instance=cart, data=request.data)
             if serializer.is_valid():
+                quantity = serializer.validated_data.get('quantity')
+                if quantity > cart.quantity or quantity < cart.quantity:
+                    p = quantity * cart.food.price
+                    cart.finall_price = p
                 serializer.save()
                 return Response({'status': 'cart is successfully update'}, status.HTTP_200_OK)
         else:
@@ -128,8 +132,8 @@ class CartAddApiView(APIView):
                     quantity = 1
                 else:
                     quantity = request.data.get('quantity')
-
-                create_cart = CartModel.objects.create(user=request.user, food=food, quantity=quantity)
+                p = total_price(quantity=quantity, price=food.price)
+                create_cart = CartModel.objects.create(user=request.user, food=food, quantity=quantity, finall_price=p)
                 create_cart.save()
                 return Response({'status': 'cart succecful created'}, status.HTTP_201_CREATED)
         else:
